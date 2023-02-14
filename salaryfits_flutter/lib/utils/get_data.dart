ffcd4a56f89308e16aaaa3c6b6c50520d2a48090
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-02-13 23:18:47 -0300

    feat(post) post data into firabase realtime database

--- salaryfits_flutter/lib/utils/get_data.dart
+++ salaryfits_flutter/lib/utils/get_data.dart
@@ -3,11 +3,15 @@ import 'package:http/http.dart' as http;
 
 class Get {
   Future<List> execute() async {
+    var firestoneDb = Uri.parse(
+        'https://salaryfits-ps-default-rtdb.firebaseio.com/data.json');
     var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-    var response =  await http.get(url);
+    var response = await http.get(url);
 
     if (response.statusCode == 200) {
-      return jsonDecode(utf8.decode(response.bodyBytes));
+      var data = jsonDecode(utf8.decode(response.bodyBytes));
+      await http.post(firestoneDb, body: response.body);
+      return data;
     } else {
       throw Exception('Request failed with status: ${response.statusCode}.');
     }
